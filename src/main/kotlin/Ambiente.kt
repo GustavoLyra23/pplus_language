be7bb8c627e35ece7a536bec0e61a2d5252738ed
GustavoLyra23 <gustavolyra23@gmail.com>
2025-05-14 17:27:53 -0300

    refactor interpreter logic and remove unused code; add sample loop in P++ script

--- src/main/kotlin/Ambiente.kt
+++ src/main/kotlin/Ambiente.kt
@@ -5,7 +5,62 @@ import org.gustavolyra.portugolpp.PortugolPPParser.DeclaracaoClasseContext
 class Ambiente(val enclosing: Ambiente? = null) {
     private val valores = mutableMapOf<String, Valor>()
     private val classes = mutableMapOf<String, DeclaracaoClasseContext>()
+    private val interfaces = mutableMapOf<String, PortugolPPParser.DeclaracaoInterfaceContext>()
     var thisObjeto: Valor.Objeto? = null
+
+
+    fun definirInterface(nome: String, declaracao: PortugolPPParser.DeclaracaoInterfaceContext) {
+        interfaces[nome] = declaracao
+    }
+
+    fun obterInterface(nome: String): PortugolPPParser.DeclaracaoInterfaceContext? {
+        return interfaces[nome] ?: enclosing?.obterInterface(nome)
+    }
+
+    fun getInterfaces(classeContext: DeclaracaoClasseContext): List<String> {
+        val result = mutableListOf<String>()
+        var foundImplements = false
+
+        for (i in 0 until classeContext.childCount) {
+            val token = classeContext.getChild(i).text
+
+            if (foundImplements) {
+                if (token == "{") break
+                if (token != "," && token != "implementa") {
+                    result.add(token)
+                }
+            } else if (token == "implementa") {
+                foundImplements = true
+            }
+        }
+
+        return result
+    }
+
+    fun getSuperClasse(classeContext: DeclaracaoClasseContext): String? {
+        for (i in 0 until classeContext.childCount) {
+            if (classeContext.getChild(i).text == "estende" && i + 1 < classeContext.childCount) {
+                return classeContext.getChild(i + 1).text
+            }
+        }
+        return null
+    }
+
+    fun verificaImplementacaoInterface(classeDecl: DeclaracaoClasseContext, interfaceNome: String): Boolean {
+        val interfaceDec1 = obterInterface(interfaceNome) ?: return false
+        for (assinatura in interfaceDec1.assinaturaMetodo()) {
+            val nomeMetodo = assinatura.ID().text
+            val metodoImplementado = classeDecl.declaracaoFuncao().find {
+                it.ID().text == nomeMetodo
+            }
+            if (metodoImplementado == null) {
+                return false
+            }
+        }
+
+        return true;
+    }
+
 
     fun definir(nome: String, valor: Valor) {
         valores[nome] = valor
